a8542119b190fda1512abc6336e64b2e998c4ca9
jai-sharma <sendtojsharma@gmail.com>
2020-10-14 10:38:50 +0530

    added signup componet

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Signup from './Signup';
 
 function App() {
-  return (
-    <div className="App">
-      hello, react
-    </div>
-  );
+    return (
+        <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: '100vh' }}>
+            <div className="w-100" style={{ maxWidth: '400px' }}>
+                <Signup />
+            </div>
+        </Container>
+    );
 }
 
 export default App;
